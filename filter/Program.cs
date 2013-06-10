d0de9eddb13a43fd64b138171c761cc0afb65f49
Petr Bena <benapetr@gmail.com>
2013-06-10 16:57:29 +0200

    gdsgs

--- filter/Program.cs
+++ filter/Program.cs
@@ -9,6 +9,22 @@ namespace filter
     {
         static void Main(string[] args)
         {
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                if (s.StartsWith("tools-webserver") && s.Contains("HTTP/"))
+                {
+                    if (s.Contains("+0000]"))
+                    {
+                        int position = 0;
+                        position = s.IndexOf("+0000]") + 8;
+                        int position_of_first = s.IndexOf("/", position);
+                        int position_of_next = s.IndexOf("/", position_of_first + 1);
+                        s = s.Substring(0, position_of_next) + "HTTP" + s.Substring(s.LastIndexOf("/"));
+                        Console.WriteLine(s);
+                    }
+                }
+            }
         }
     }
 }
